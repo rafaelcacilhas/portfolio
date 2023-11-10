e04d09f568338ebaa546962eac49e1914a963485
Rafael Cacilhas <rafael.cacilhas@quorum.us>
2023-11-10 16:44:19 -0300

    idk, a lot of work

---
+++ src/components/Curriculum.svelte
@@ -0,0 +1,56 @@
+<script>
+	let mainColor = '#F76982';
+	let backgroundColor = '#1F2937'
+
+</script>
+
+<section>
+
+	<p class="title">Curriculum - or the road so far</p>
+
+	<div class="container">
+
+		<div class="section">
+				<p class="sectionTitle">Education</p>
+				<p class="sectionContent"> Some more stuff goes here Some more stuff Some more stuffSome more stuffSome more stuffSome more stuffSome more stuff</p>
+				<p class="sectionContent"> Some more stuff goes here Some more stuff Some more stuffSome more stuffSome more stuffSome more stuffSome more stuff</p>
+
+		</div>
+		<div class="section">
+				<p class="sectionTitle">Experience</p>
+				<p class="sectionContent"> Some more stuff </p>
+
+		</div>
+
+		<div class="section">
+			<p class="sectionTitle">Skills</p>
+			<p class="sectionContent"> Some more stuff </p>
+
+		</div>
+
+	</div>
+
+</section>
+
+<style>
+	.title {
+		font-size: 2em;
+	}
+	.container {
+		border: 1px solid #F76982;
+		border-radius: 1rem;
+		background-color: #1F2937;
+		padding: 1rem;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+	.sectionTitle{
+		color: #F76982
+	}
+	.sectionContent{
+		font-size: medium;
+	}
+
+
+</style>
---
+++ src/data/blog-posts/c-stuff.md
@@ -0,0 +1,9 @@
+---
+title: C stuff
+publishDate: 30 Sept 2023
+description: My adventures on C land
+---
+
+![Illustration of woman using a meditation app](/assets/blog/casual-life-3d-meditation-crystal.webp)
+
+This is a post about C
