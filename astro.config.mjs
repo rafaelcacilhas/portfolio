3d33d2a57581c8f41700c3c0f0b85976bb540fb5
Rafael Cacilhas <rafaelcacilhas@gmail.com>
2026-02-24 10:58:53 -0300

    Fix submmodule css by adding tailwind as vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import smartypants from 'remark-smartypants'
 import remarkGfm from 'remark-gfm'
 import rehypeExternalLinks from 'rehype-external-links'
 import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -34,4 +35,16 @@ export default defineConfig({
         '$lib': '/src/components/metrodrone/src/lib'
       },
     },
+    plugins: [
+          tailwindcss({
+            config: {
+              content: [
+                './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+                // Add the metrodrone submodule
+                './src/components/metrodrone/src/**/*.{svelte,js,ts}',
+                './src/components/metrodrone/**/*.{svelte,js,ts}'
+              ]
+            }
+          })
+        ]
   }})
